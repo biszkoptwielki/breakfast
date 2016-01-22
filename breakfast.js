e2c41e48f43875f9023375802365200f2c182bf4
Mikolaj Nowacki <mikolaj.nowacki@gmail.com>
2016-01-22 11:55:22 +0000

    Fix date handling

--- breakfast.js
+++ breakfast.js
@@ -1,6 +1,12 @@
 window.onload = function(e) {
-    var now = Date.now();
-    var relevantDates = dates.filter(function(a) { return a.date.getTime() > now });
+    function lastMonday() {
+        var today = new Date();
+        var day = today.getDay() === 0 ? 7 : today.getDay();
+        var monday = new Date(today.getFullYear(), today.getMonth(), today.getDate() - day + 1);
+        return monday
+    }
+
+    var relevantDates = dates.filter(function(a) { return a.date.getTime() > lastMonday().getTime() });
 
     var thisWeek = relevantDates.shift();
     var nextWeek = relevantDates.shift();
